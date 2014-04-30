dafd3952152e97c56747abab3320615d6508a976
hxtpoe <hxtpoe@gmail.com>
2014-04-30 12:40:24 +0200

    paths fix

--- public/front/app/scripts/main.js
+++ public/front/app/scripts/main.js
@@ -2,47 +2,42 @@
 require.config({
     paths: {
         angular: '../bower_components/angular/angular',
-        angularRoute: '../bower_components/angular-route/angular-route',
-        angularCookies: '../bower_components/angular-cookies/angular-cookies',
-        angularSanitize: '../bower_components/angular-sanitize/angular-sanitize',
-        angularResource: '../bower_components/angular-resource/angular-resource',
         sassbootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
         'sass-bootstrap': '../bower_components/sass-bootstrap/dist/js/bootstrap',
-        jquery: '../bower_components/jquery/dist/jquery',
         'angular-scenario': '../bower_components/angular-scenario/angular-scenario',
         'angular-sanitize': '../bower_components/angular-sanitize/angular-sanitize',
         'angular-route': '../bower_components/angular-route/angular-route',
         'angular-resource': '../bower_components/angular-resource/angular-resource',
         'angular-mocks': '../bower_components/angular-mocks/angular-mocks',
         'angular-cookies': '../bower_components/angular-cookies/angular-cookies'
-    },
-    shim: {
-        angular: {
+      },
+      shim: {
+          angular: {
             exports: 'angular'
-        },
-        angularRoute: [
+          },
+          'angular-route': [
             'angular'
-        ],
-        angularCookies: [
+          ],
+          'angular-cookies': [
             'angular'
-        ],
-        angularSanitize: [
+          ],
+          'angular-sanitize': [
             'angular'
-        ],
-        angularResource: [
+          ],
+          'angular-resource': [
             'angular'
-        ],
-        angularMocks: {
+          ],
+          'angular-mocks': {
             deps: [
                 'angular'
-            ],
-            exports: 'angular.mock'
-        }
-    },
-    priority: [
-        'angular'
-    ]
-});
+              ],
+              exports: 'angular.mock'
+            }
+          },
+          priority: [
+            'angular'
+          ]
+        });
 
 //http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
 window.name = 'NG_DEFER_BOOTSTRAP!';
@@ -50,10 +45,10 @@ window.name = 'NG_DEFER_BOOTSTRAP!';
 require([
     'angular',
     'app',
-    'angularRoute',
-    'angularCookies',
-    'angularSanitize',
-    'angularResource'
+    'angular-route',
+    'angular-cookies',
+    'angular-sanitize',
+    'angular-resource'
   ], function (angular, app, ngRoutes, ngCookies, ngSanitize, ngResource) {
     'use strict';
     /* jshint ignore:start */
