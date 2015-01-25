3a7e52db6c4a2657fb0cd86c9d15a893998fa953
hxtpoe <hxtpoe@gmail.com>
2015-01-25 21:01:51 +0100

    return correct JWT token

--- app/services/JWTservice.scala
+++ app/services/JWTservice.scala
@@ -2,6 +2,10 @@ package services
 
 import akka.actor.{Props, Actor}
 import models.User
+import com.nimbusds.jose._
+import com.nimbusds.jose.crypto._
+import com.nimbusds.jwt._
+
 
 class JWTService extends Actor {
   import JWTService._
@@ -13,6 +17,18 @@ class JWTService extends Actor {
 
 object JWTService {
   case class Generate(user: User)
+
+  def generate(subject: String): String = {
+    // Create HMAC signer
+    val signer = new MACSigner("xx")
+
+    // Prepare JWT with claims set
+    val claimsSet = new JWTClaimsSet()
+    claimsSet.setSubject(subject)
+    val signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+    signedJWT.sign(signer);
+    signedJWT.serialize();
+  }
 
   def props = Props(new JWTService)
 }
