c7e747852bfa8b0bd9a85c458e3f8ae334176321
hxtpoe <hxtpoe@gmail.com>
2015-01-25 01:58:45 +0100

    fix cors

--- app/filters/CorsFilter.scala
+++ app/filters/CorsFilter.scala
@@ -8,6 +8,7 @@ object CorsFilter extends Filter {
   override def apply(next: RequestHeader => Future[Result])(request: RequestHeader): Future[Result] = {
     next(request).map {
       result => result.withHeaders("Access-Control-Allow-Origin" -> "*",
+        "Allow" -> "*",
         "Access-Control-Allow-Methods" -> "POST, GET, OPTIONS, PUT, DELETE",
         "Access-Control-Allow-Headers" -> "x-requested-with, Content-Type, origin, authorization, accept, client-security-token"
       )
