b277a24c5e637aae208a664568e2e55510f68abe
hxtpoe <hxtpoe@gmail.com>
2015-01-28 23:38:06 +0100

    allow if-none-match

--- app/controllers/Cors.scala
+++ app/controllers/Cors.scala
@@ -9,6 +9,6 @@ object Cors extends Controller {
       "Access-Control-Allow-Origin" -> "*",
       "Allow" -> "*",
       "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers" -> "x-requested-with, Content-Type, token, origin, authorization, accept, client-security-token")
+      "Access-Control-Allow-Headers" -> "x-requested-with, Content-Type, token, origin, if-none-match, authorization, accept, client-security-token")
   }
 }
